e988a9eba6aa236ac005788038ed4e0c7a1f04c8
Mallchad <ncaprisunfan@gmail.com>
2023-11-01 05:31:57 +0000

    [cpp_opengl_triangle] Maintainance: Added aparently missing includes from earlier commit

---
+++ cpp_opengl_triangle/include_tracy.h
@@ -0,0 +1,13 @@
+
+// Don't want to debug other programs unless something is broken, disable warnings
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#pragma clang diagnostic ignored "-Wextra-semi"
+#pragma clang diagnostic ignored "-Wold-style-cast"
+#pragma clang diagnostic ignored "-Wpadded"
+#pragma clang diagnostic ignored "-Wsuggest-destructor-override"
+#pragma clang diagnostic ignored "-Wextra-semi-stmt"
+
+#include "Tracy.hpp"
+#pragma clang diagnostic pop
---
+++ cpp_opengl_triangle/main_include.h
@@ -0,0 +1,12 @@
+
+// Standard Lirary
+#include <iostream>
+#include <bits/unique_ptr.h>
+#include <bits/this_thread_sleep.h>
+#include <chrono>
+
+// Project Local
+#include "global.h"
+#include "renderer.h"
+
+#include "include_tracy.h"
