3b05f8d6944f5c8775c6d0c84bbd7d1bf1d8823a
Mallchad <ncaprisunfan@gmail.com>
2024-06-22 14:51:00 +0100

    Added: Initial catch2 framework

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,12 +1,25 @@
 
 #include "file.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
 
-int main()
+    TEST_CASE( "File Handling Library" )
+    {
+        REQUIRE( file_load_binary( "/mnt/tmp/repos/experiments_rendering/assets/empty_file.bin" )
+                    .size() >= 0 );
+
+
+    }
+
+int main( int argc, char** argv )
 {
     file_load_binary( "/mnt/tmp/repos/experiments_rendering/assets/empty_file.bin" );
+    int result = Catch::Session().run( argc, argv );
+
+    return result;
 }
 
-int test_main()
+int test_main( int argc, char** argv )
 {
-    return main();
+    return main( argc, argv );
 }
--- xmake.lua
+++ xmake.lua
@@ -68,6 +68,75 @@ target( "tachyon_engine" )
 
                    )
 
+target( "tachyon_libs" )
+    set_kind( "static" )
+    set_languages( "c++20" )
+    add_packages("glslang")
+
+    set_toolchains( "clang" )
+    set_policy("build.ccache", true)
+    add_files( "source/error.cpp",
+               "source/global.cpp",
+               "source/file.cpp",
+               "source/time.cpp" )
+
+    add_includedirs( "../tracy",
+                     "external/spdlog/include/",
+                     "source" )
+
+    -- asan must be linked first
+    -- add_links( "asan" )
+    -- add_links( "ubsan" )
+    add_links( "dl", "X11", "GL" )
+    add_defines( 'TRIANGULATE_PROJECT_ROOT="$(projectdir)"' )
+
+    -- Temporary cxxflags to safe the effort of full converting to xmake --
+    -- Using lld linker instead of mold for now for error messages
+    -- Reconsider if the build gets slow
+    add_cxxflags( "clang::-g",
+                  -- "-fuse-ld=lld",
+                  "-fuse-ld=mold",
+
+                  -- Warnings
+                  "-Wpedantic",
+                  "-Wall",
+                  -- Whitelist Errors
+                  -- #import is a Obj-C language extension and easily confused with C++ modules
+                  "-Werror=import-preprocessor-directive-pedantic",
+                  -- Platform unknown pragmas lead to some insanely annoying and
+                  -- unexpected When they fail silentlys
+                  "-Werror=unknown-pragmas",
+                  -- Non-Void functions that don't return can crash *at runtime*
+                  -- "-Werror=return-type",
+                  "-Werror=inconsistent-missing-override",
+                  -- Shadowing
+                  "-Werror=shadow-all",
+                  -- Has inconsistent behaviour and behaves like reinterpret cast, never use
+                  "-Werror=old-style-cast",
+
+                  -- -- Disable Warnings
+                  "-Wno-unused-value",
+                  "-Wno-padded",
+                  "-Wno-c++98-compat",
+                  "-Wno-c++98-compat-pedantic ",
+                  "-Wno-documentation-unknown-command ",
+                  "-Wno-unreachable-code-break",
+                  "-Wno-unused-variable",
+                  "-Wno-unused-private-field",
+                  "-Wno-abstract-final-class",
+                  -- Only runs on extra semicolons that do nothing, pointless.
+                  "-Wno-extra-semi-stmt"
+                  -- "-fsanitize=address",
+                  -- "-fsanitize=thread",
+                  -- "-fsanitize=memory",
+                  -- "-fsanitize=undefined",
+                  -- "-fsanitize=dataflow",
+                  -- "-fsanitize=cfi",   -- Control Flow Integrity
+                  -- "-fsanitize=kcfi",  -- Kernel Indirect Call Forward-Edge Control Flow Integrity
+                  -- "-fsanitize=safe-stack",
+
+                   )
+
     if is_mode( "release" ) then
        add_cxxflags( "clang::-g",
                      "-Weverything", "-Wno-implicit-int-float-conversion",
@@ -85,7 +154,28 @@ target( "tachyon_engine" )
                      "-Werror=shadow" )
     end
 
-target(shaders)
+target( "shaders" )
+    set_kind( "phony" )
     add_rules("utils.glsl2spv", {outputdir = "build"})
     add_files("source/shaders/*.vert",
               "source/shaders/*.frag")
+
+target( "tachyon_tests" )
+    set_kind( "binary" )
+    set_languages( "c++20" )
+    add_deps( "tachyon_libs" )
+
+    add_tests( "catch2" )
+    add_links( "build/linux/x86_64/release/libtachyon_libs.a",
+               "external/catch2/build/src/catch2/libCatch2.a" )
+    add_files( "tests/*.cpp" )
+    add_includedirs( "../tracy",
+                     "external/spdlog/include/",
+                     "external/catch2/src/",
+                     "tests/",
+                     "source" )
+
+    add_defines( 'TRIANGULATE_PROJECT_ROOT="$(projectdir)"' )
+
+target( "catch2" )
+    set_kind( "phony" )
