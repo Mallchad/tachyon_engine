5a95ff066bf072c585fc253df9e5212de1ccc954
Mallchad <ncaprisunfan@gmail.com>
2024-07-18 14:44:40 +0100

    Added: Missing error cpp file

---
+++ source/error.cpp
@@ -0,0 +1,19 @@
+
+#include "error.hpp"
+
+/// Runtime assertion that raises an exception when false
+// Aims to improve over the built-in assert by not adhereing to DEBUG/NDEBUG macros
+// and being more transparently configurable.
+void
+FUNCTION assertion( bool expression, const char* message )
+{
+    print( message );
+    std::raise( SIGABRT );
+}
+
+void
+FUNCTION unimplimented( cstring reason )
+{
+    print( "This code path is unimplimented, reasoning is as follows " );
+    assertion( true, reason );
+}
