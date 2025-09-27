10853f4cde7f8d594a124d18d1ef8c49447c2576
Mallchad <ncaprisunfan@gmail.com>
2025-09-27 19:41:44 +0100

    Added: Experimental Unity build file

---
+++ source/build_control/unity.cpp
@@ -0,0 +1,54 @@
+
+#include "include_core.h"
+
+
+    #include "../error.cpp"
+
+
+    #include "../file.cpp"
+
+
+    #include "../global.cpp"
+
+
+    #include "../input.cpp"
+
+
+    #include "../linux_input_xlib.cpp"
+
+
+    #include "../linux_opengl.cpp"
+
+
+    #include "../math.cpp"
+
+
+    #include "../memory.cpp"
+
+
+    #include "../platform_linux_x11.cpp"
+
+
+    #include "../platform_vulkan.cpp"
+
+
+    #include "../renderer.cpp"
+
+
+    #include "../string.cpp"
+
+
+    #include "../table.cpp"
+
+
+    #include "../time.cpp"
+
+
+#include "external/tachyon_lib/source/tachyon_library.cpp"
+#include "external/tachyon_lib/source/tachyon_platform_linux.cpp"
+#include "external/tachyon_lib/source/tachyon_platform_windows.cpp"
+
+// #include "external/lua/onelua.c"
+
+// Main include
+#include "../main.cpp"
