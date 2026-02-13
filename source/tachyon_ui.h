34f470e840e0e727c572ad286c535dbbddc99879
Mallchad <ncaprisunfan@gmail.com>
2026-02-13 22:46:05 +0000

    Maintenance: Formatting change in tachyon_ui.h

--- source/tachyon_ui.h
+++ source/tachyon_ui.h
@@ -3,35 +3,40 @@
 
 namespace tyon
 {
-    template<>
-    struct entity_type_definition<window>
-    {
-        using t_entity = window;
-        using t_context = entity_type_context<t_entity>;
-        static constexpr cstring name = "tyon::window";
-        static constexpr u128 id = uuid("965fd378-6573-4844-aa34-e6645cc3ac7a");
+template<>
+struct entity_type_definition<window>
+{
+  using t_entity = window;
+  using t_context = entity_type_context<t_entity>;
+  static constexpr cstring name = "tyon::window";
+  static constexpr u128 id = uuid("965fd378-6573-4844-aa34-e6645cc3ac7a");
 
-        PROC allocate() -> void
-        {}
-        PROC init( t_entity* arg ) -> fresult
-        {
-            return false;
-        }
-        PROC destroy( t_entity* arg ) -> void
-        {
-            auto sdl = sdl_create_platform_subsystem();
-            sdl.window_close( arg );
-            *arg = {};
-        }
+PROC allocate() -> void
+{}
 
-        PROC tick() -> void
-        {}
+PROC init( t_entity* arg ) -> fresult
+{
+  return false;
+}
+PROC destroy( t_entity* arg ) -> void
+{
+  auto sdl = sdl_create_platform_subsystem();
+  sdl.window_close( arg );
+  *arg = {};
+}
 
-        static PROC context_tick( void* context ) -> void
-        {}
+PROC tick() -> void
+{}
 
-        static PROC destroy_all( void* context ) -> void
-        {}
+static PROC context_tick( void* context ) -> void
+{}
 
-    };
+static PROC destroy_all( void* context ) -> void
+{}
+
+};
+
+struct ui_frame
+{
+};
 }
